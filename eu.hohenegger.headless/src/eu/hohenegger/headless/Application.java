9b662e364c07b482d2a7e7d28abbde01473a6798
Max Hohenegger <github.com@hohenegger.eu>
2015-03-11 00:11:53 +0100

    implemented basic repl cli

--- eu.hohenegger.headless/src/eu/hohenegger/headless/Application.java
+++ eu.hohenegger.headless/src/eu/hohenegger/headless/Application.java
@@ -1,26 +1,59 @@
 package eu.hohenegger.headless;
 
 import java.util.Map;
+import java.util.Scanner;
+
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 
 public class Application implements IApplication {
-	private static final String APPLICATION_ARGS = "application.args";
+//	private static final String APPLICATION_ARGS = "application.args";
 
 	@Override
 	public Object start(final IApplicationContext context) throws Exception {
 
 		@SuppressWarnings("unchecked")
 		final Map<String, Object> args = context.getArguments();
-		String[] appArgs = (String[]) args.get(APPLICATION_ARGS);
-
-		for (String string : appArgs) {
-			System.out.println(string);
-		}
+//		String[] appArgs = (String[]) args.get(APPLICATION_ARGS);
 
 		waitForGoshCommandToBeRegistered();
+		
+		startRepl();
 
 		return IApplication.EXIT_OK;
+	}
+
+	private static final String fNEW_LINE = System.getProperty("line.separator");
+
+	private static String getInheritanceTree(Class<?> aClass) {
+		StringBuilder superclasses = new StringBuilder();
+		superclasses.append(fNEW_LINE);
+		Class<?> theClass = aClass;
+		while (theClass != null) {
+			superclasses.append(theClass);
+			superclasses.append(fNEW_LINE);
+			theClass = theClass.getSuperclass();
+		}
+		superclasses.append(fNEW_LINE);
+		return superclasses.toString();
+	}
+
+	private void startRepl() {
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a package-qualified class name:");
+		while (sc.hasNextLine()) {
+			String command = sc.nextLine();
+			if ("exit".equals(command)) {
+				break;
+			}
+			try {
+				Class<?> theClass = Class.forName(command);
+				System.out.println("The inheritance tree: " + getInheritanceTree(theClass));
+			} catch (ClassNotFoundException e) {
+				 System.out.println("Cannot find that class.");
+			}
+		}
+		sc.close();
 	}
 
 	private void waitForGoshCommandToBeRegistered() {
